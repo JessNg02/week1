0a9213c149ca09d49cadecd1fd077b6a2494b71d
Jessica <jessicazmng@gmail.com>
2020-07-30 19:21:03 -0400

    whatever I want

---
+++ day2.js
@@ -0,0 +1,78 @@
+/*
+  A student scored
+  25/50 in math,
+  30/50 in english,
+  14/50 in Physics,
+  48/50 in Chemistry
+  and 50/50 in Accounting
+  Calculate and print out the average.
+  Also calculate and print out overall percentage.
+
+  1. Navigate into folder that contains the file 
+  2. In the terminal write node + [file name]
+*/
+ 
+/*let math = ((25/50)*100)
+let english = ((30/50)*100)
+let physics = ((14/50)*100)
+let chem = ((48/50)*100)
+let account = ((50/50)*100)
+let result = (math + english + physics + chem + account)/5
+console.log ("average is " + result)
+console.log ("percentage is " + result + "%")
+/*
+
+/*
+Given 5 numbers, find and print the largest one.
+*/
+ 
+/*
+let num1 = -900;
+let num2 = 13.3298;
+let num3 = 23;
+let num4 = 1.0009;
+let num5 = 32;
+ 
+console.log(Math.max(num1, num2, num3, num4, num5))
+*/
+
+/*
+  Find and log the sign of the product of three numbers.
+  Example:
+    Sample numbers : 3, -7, 2
+    Output : The sign is -
+*/
+
+/*let num1 = 78;
+let num2 = -56;
+let num3 = -900;
+ 
+let result = (num1*num2*num3)
+ if (result > 0)
+   console.log("The sign is + ")
+   else if (result < 0)
+   console.log("The sign is - ")
+*/
+
+/*
+  Given 2 pairs of numbers, calculate and log the sum of their products.
+  Example:
+    (assume the first 2 numbers are a pair and the last 2 are a pair)
+    Sample numbers : 5, 10, 3, 6
+    Output : The sum of products is 68
+*/
+ 
+/*
+let num1 = 5;
+let num2 = 10;
+let num3 = 3;
+let num4 = 6;
+ 
+let pair1 =(num1*num2)
+let pair2 =(num3*num4)
+console.log(pair1 + pair2)
+
+*/
+
+console.log("hello")
+
