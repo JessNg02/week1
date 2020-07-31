66fafa0d39a45e4dfd6765ceaada05bcb719181e
Jessica <jessicazmng@gmail.com>
2020-07-31 09:58:35 -0400

    day 3 question 2

---
+++ day3q2.js
@@ -0,0 +1,13 @@
+/*
+Let’s try 5 array operations.
+Create an array styles with items “Jazz” and “Blues”.
+Append (add) “Rock-n-Roll” to the end.
+Replace the value in the middle by “Classics”. Your code for finding the middle value should work for any arrays with odd length.
+Strip off the first value of the array and show it.
+Prepend Rap and Reggae to the array.
+Console.log every step of the way
+*/
+
+let music =["Jazz", "Blues"]
+music.push("Rock-n-Roll")
+console.log(music)
